639265aa3ba1a8f057d9eb3501d093439f6943f3
Ron Guglielmone <ronniedaymusic@gmail.com>
2017-05-20 21:57:59 -0700

    Create ee178Pset3.m

---
+++ EE178_Probabilistic_Systems_Analysis/ee178Pset3.m
@@ -0,0 +1,54 @@
+% Number of tosses:
+n = 1000;
+
+% Trial results:
+nFor0 = 0;
+nFor1 = 0;
+nFor2 = 0;
+nFor3 = 0;
+
+
+for i = 0:n
+    
+    toss1 = randi([0 1]);
+    toss2 = randi([0 1]);
+    toss3 = randi([0 1]);
+    toss4 = randi([0 1]);
+    toss5 = randi([0 1]);
+    
+    count = 0;
+    
+    if toss1 - toss2 == 1
+        count = count+1;
+    end
+    
+    if toss2 - toss3 == 1
+        count = count+1;
+    end
+    
+    if toss3 - toss4 == 1
+        count = count+1;
+    end
+    
+    if toss4 - toss5 == 1
+        count = count+1;
+    end
+    
+    if count == 0
+        nFor0 = nFor0 + 1;
+    end
+    if count == 1
+        nFor1 = nFor1 + 1;
+    end
+    if count == 2
+        nFor2 = nFor2 + 1;
+    end
+    if count == 3
+        nFor3 = nFor3 + 1;
+    end
+end
+
+fprintf('Number of 0 results: %g \n',nFor0);
+fprintf('Number of 1 results: %g \n',nFor1);
+fprintf('Number of 2 results: %g \n',nFor2);
+fprintf('Number of 3 results: %g \n',nFor3);
