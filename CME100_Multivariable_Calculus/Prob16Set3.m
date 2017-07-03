ffc4908877b1175c3b931d95440c08e668161334
Ron Guglielmone <ronniedaymusic@gmail.com>
2017-07-02 17:56:49 -0700

    Update Prob16Set3.m

--- CME100_Multivariable_Calculus/Prob16Set3.m
+++ CME100_Multivariable_Calculus/Prob16Set3.m
@@ -1,3 +1,13 @@
+% Author: Ron Guglielmone
+% Language: MATLAB R2015
+% Date: Fall 2015
+%
+% Course: CME100
+% School: Stanford University
+% Problem Set X, Problem No. X
+%
+% Description: X
+
 % Time Step:
 dt = 0.1;
 t = 0:dt:2*pi;
